eeede4af199ad7f67263c35d2d9ca14d1520c728
KoofNG <israelakpan296@gmail.com>
2019-09-05 09:41:11 +0100

    Successfully created the Initial Project

---
+++ src/test/java/com/springing/SpringingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
